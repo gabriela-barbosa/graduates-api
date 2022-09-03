09b9ac0f04671ee6b8fe912ab8108bbd2f9e26d5
gabriela-barbosa <gabrielabf@id.uff.br>
2022-09-03 17:04:37 -0300

    Trying to add pagination

--- src/main/kotlin/br/uff/graduatesapi/repository/GraduateRepository.kt
+++ src/main/kotlin/br/uff/graduatesapi/repository/GraduateRepository.kt
@@ -2,6 +2,8 @@ package br.uff.graduatesapi.repository
 
 import br.uff.graduatesapi.model.Advisor
 import br.uff.graduatesapi.model.Graduate
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface GraduateRepository : JpaRepository<Graduate, Int> {
@@ -9,6 +11,6 @@ interface GraduateRepository : JpaRepository<Graduate, Int> {
   fun findAllByOrderByUserNameAsc(): List<Graduate>
   fun findAllByWorkHistoryInstitutionId(institutionId: Int): List<Graduate>
   fun findAllByCoursesAdvisorIsOrderByHistoryStatusDesc(
-    advisor: Advisor
-  ): MutableList<Graduate>?
+    advisor: Advisor, pageable: Pageable
+  ): Page<Graduate>
 }
