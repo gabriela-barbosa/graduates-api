a898f96c9c777f87725faf8a73faf3d4a67cb127
Gabriela Barbosa <gabriela.barbosa@lend.tech>
2023-05-28 16:04:59 -0300

    chore: update populate.sql

--- db/populate.sql
+++ db/populate.sql
@@ -1,156 +1,185 @@
 insert into platform_user (id, email, name, password, created_at)
-values (gen_random_uuid(), 'gabriela@gmail.com', 'Gabriela',
+values ('e668b740-5499-4caf-a5a2-0431ef0a5444', 'gabriela@gmail.com', 'Gabriela',
         '$2a$10$j4fJVllmbSalkl1.M0bDE.Fm2sqjRj8r50pykWQVd35TIqFwlPX0u', now()),
-       (gen_random_uuid(), 'davi@gmail.com', 'Davi', '$2a$10$j4fJVllmbSalkl1.M0bDE.Fm2sqjRj8r50pykWQVd35TIqFwlPX0u',
+       ('374570af-f7e5-4585-9815-942c20746f06', 'davi@gmail.com', 'Davi',
+        '$2a$10$j4fJVllmbSalkl1.M0bDE.Fm2sqjRj8r50pykWQVd35TIqFwlPX0u',
         now()),
-       (gen_random_uuid(), 'victor@gmail.com', 'Victor', '$2a$10$j4fJVllmbSalkl1.M0bDE.Fm2sqjRj8r50pykWQVd35TIqFwlPX0u',
+       ('cf4dedca-1f1b-43dd-af71-3b0f25cb9b9c', 'victor@gmail.com', 'Victor',
+        '$2a$10$j4fJVllmbSalkl1.M0bDE.Fm2sqjRj8r50pykWQVd35TIqFwlPX0u',
         now()),
-       (gen_random_uuid(), 'arthur@gmail.com', 'Arthur', '$2a$10$j4fJVllmbSalkl1.M0bDE.Fm2sqjRj8r50pykWQVd35TIqFwlPX0u',
+       ('bee99937-97bb-4d0d-8219-5ac586c6c712', 'arthur@gmail.com', 'Arthur',
+        '$2a$10$j4fJVllmbSalkl1.M0bDE.Fm2sqjRj8r50pykWQVd35TIqFwlPX0u',
         now()),
-       (gen_random_uuid(), 'luiza@gmail.com', 'Luiza', '$2a$10$j4fJVllmbSalkl1.M0bDE.Fm2sqjRj8r50pykWQVd35TIqFwlPX0u',
+       ('e69e3415-b1fa-4430-bdca-886fe5bc9af4', 'luiza@gmail.com', 'Luiza',
+        '$2a$10$j4fJVllmbSalkl1.M0bDE.Fm2sqjRj8r50pykWQVd35TIqFwlPX0u',
         now()),
-       (gen_random_uuid(), 'vanessa@gmail.com', 'Vanessa',
+       ('59d8c94f-e5b5-4259-b0d3-2d26758e7eaa', 'vanessa@gmail.com', 'Vanessa',
         '$2a$10$j4fJVllmbSalkl1.M0bDE.Fm2sqjRj8r50pykWQVd35TIqFwlPX0u', now()),
-       (gen_random_uuid(), 'gabriel@gmail.com', 'Gabriel',
+       ('b9306ad8-9713-4b9c-ad87-39570e4e8b85', 'gabriel@gmail.com', 'Gabriel',
         '$2a$10$j4fJVllmbSalkl1.M0bDE.Fm2sqjRj8r50pykWQVd35TIqFwlPX0u', now()),
-       (gen_random_uuid(), 'anselmo@gmail.com', 'Anselmo',
+       ('9b6faf8f-c187-4198-82f4-93236fc26446', 'anselmo@gmail.com', 'Anselmo',
         '$2a$10$j4fJVllmbSalkl1.M0bDE.Fm2sqjRj8r50pykWQVd35TIqFwlPX0u', now()),
-       (gen_random_uuid(), 'leonardo@gmail.com', 'Leonardo',
+       ('2d5da6fc-9fb4-45a1-9e52-5e20a04a3c43', 'leonardo@gmail.com', 'Leonardo',
         '$2a$10$j4fJVllmbSalkl1.M0bDE.Fm2sqjRj8r50pykWQVd35TIqFwlPX0u', now()),
-       (gen_random_uuid(), 'plastino@gmail.com', 'Plastino',
+       ('d79b4d52-00ba-4fc9-985d-f00642c131a9', 'plastino@gmail.com', 'Plastino',
         '$2a$10$j4fJVllmbSalkl1.M0bDE.Fm2sqjRj8r50pykWQVd35TIqFwlPX0u', now()),
-       (gen_random_uuid(), 'troy@gmail.com', 'Troy', '$2a$10$j4fJVllmbSalkl1.M0bDE.Fm2sqjRj8r50pykWQVd35TIqFwlPX0u',
+       ('39d1393f-6b0c-479f-8e54-be5785c7db36', 'troy@gmail.com', 'Troy',
+        '$2a$10$j4fJVllmbSalkl1.M0bDE.Fm2sqjRj8r50pykWQVd35TIqFwlPX0u',
         now()),
-       (gen_random_uuid(), 'aline@gmail.com', 'Aline', '$2a$10$j4fJVllmbSalkl1.M0bDE.Fm2sqjRj8r50pykWQVd35TIqFwlPX0u',
+       ('d852e774-8199-4824-8cdb-bf092a00e96c', 'aline@gmail.com', 'Aline',
+        '$2a$10$j4fJVllmbSalkl1.M0bDE.Fm2sqjRj8r50pykWQVd35TIqFwlPX0u',
         now())
 ;
 
 
 insert into platform_user (id, email, name, password, created_at)
-values (gen_random_uuid(), 'gab@gmail.com', 'Gabrielly', '$2a$10$j4fJVllmbSalkl1.M0bDE.Fm2sqjRj8r50pykWQVd35TIqFwlPX0u',
+values ('3ea29a7d-15de-40b0-b79c-fc440d5f85e4', 'gab@gmail.com', 'Gabrielly',
+        '$2a$10$j4fJVllmbSalkl1.M0bDE.Fm2sqjRj8r50pykWQVd35TIqFwlPX0u',
         now()),
-       (gen_random_uuid(), 'davidson@gmail.com', 'Davidson',
+       ('5f1545bc-7435-4468-a365-e8a625a02fab', 'davidson@gmail.com', 'Davidson',
         '$2a$10$j4fJVllmbSalkl1.M0bDE.Fm2sqjRj8r50pykWQVd35TIqFwlPX0u', now()),
-       (gen_random_uuid(), 'viktor@gmail.com', 'Viktor', '$2a$10$j4fJVllmbSalkl1.M0bDE.Fm2sqjRj8r50pykWQVd35TIqFwlPX0u',
+       ('fdcb1d4d-636e-429c-b7ba-ecc7af3ef05e', 'viktor@gmail.com', 'Viktor',
+        '$2a$10$j4fJVllmbSalkl1.M0bDE.Fm2sqjRj8r50pykWQVd35TIqFwlPX0u',
         now()),
-       (gen_random_uuid(), 'artu@gmail.com', 'Artu', '$2a$10$j4fJVllmbSalkl1.M0bDE.Fm2sqjRj8r50pykWQVd35TIqFwlPX0u',
+       ('041f4fab-89c4-4198-87c9-7fd1504eeb6d', 'artu@gmail.com', 'Artu',
+        '$2a$10$j4fJVllmbSalkl1.M0bDE.Fm2sqjRj8r50pykWQVd35TIqFwlPX0u',
         now()),
-       (gen_random_uuid(), 'louise@gmail.com', 'Louise', '$2a$10$j4fJVllmbSalkl1.M0bDE.Fm2sqjRj8r50pykWQVd35TIqFwlPX0u',
+       ('e405ae18-b349-4d2b-8156-154091b20ec4', 'louise@gmail.com', 'Louise',
+        '$2a$10$j4fJVllmbSalkl1.M0bDE.Fm2sqjRj8r50pykWQVd35TIqFwlPX0u',
         now()),
-       (gen_random_uuid(), 'valeria@gmail.com', 'Valéria',
+       ('1ba0bd22-062a-4397-abdd-226ca952d67e', 'valeria@gmail.com', 'Valéria',
         '$2a$10$j4fJVllmbSalkl1.M0bDE.Fm2sqjRj8r50pykWQVd35TIqFwlPX0u', now()),
-       (gen_random_uuid(), 'alice@gmail.com', 'Alice', '$2a$10$j4fJVllmbSalkl1.M0bDE.Fm2sqjRj8r50pykWQVd35TIqFwlPX0u',
+       ('add37e0a-2748-4fcb-bb24-149c2eac825e', 'alice@gmail.com', 'Alice',
+        '$2a$10$j4fJVllmbSalkl1.M0bDE.Fm2sqjRj8r50pykWQVd35TIqFwlPX0u',
         now())
 ;
 
 insert into platform_user_role(platform_user_id, role)
-VALUES ('fe52dbee-c4e2-46b3-b48a-2292a1a3e84b', 'GRADUATE'),
-       ('f9194fdf-43b4-4730-ae79-f529f94c7036', 'GRADUATE'),
-       ('f8070687-cf08-4b59-bcca-016e992bd5f4', 'GRADUATE'),
-       ('f6336a47-33d3-473c-921c-7818f892158d', 'GRADUATE'),
-       ('ed42630c-ff94-47da-a727-5a0316b9d320', 'GRADUATE'),
-       ('e629f2d2-49b0-49d8-93b9-ead61095c26c', 'GRADUATE'),
-       ('b706727c-786c-4daa-9e84-946451946dbc', 'PROFESSOR'),
+VALUES ('e668b740-5499-4caf-a5a2-0431ef0a5444', 'GRADUATE'),
+       ('374570af-f7e5-4585-9815-942c20746f06', 'GRADUATE'),
+       ('cf4dedca-1f1b-43dd-af71-3b0f25cb9b9c', 'GRADUATE'),
+       ('bee99937-97bb-4d0d-8219-5ac586c6c712', 'GRADUATE'),
+       ('e69e3415-b1fa-4430-bdca-886fe5bc9af4', 'GRADUATE'),
+       ('59d8c94f-e5b5-4259-b0d3-2d26758e7eaa', 'GRADUATE'),
+       ('b9306ad8-9713-4b9c-ad87-39570e4e8b85', 'PROFESSOR'),
 
-       ('aed6b967-e947-41eb-b9c5-52606176a545', 'GRADUATE'),
-       ('90a00332-666b-4072-95f0-5c16b0e34f5d', 'GRADUATE'),
-       ('88190fca-7db3-43ad-ac17-c7efa6431236', 'ADMIN'),
-       ('84bcb5f0-5d23-45a9-91a0-545d7c849bfb', 'GRADUATE'),
-       ('8005e55a-8d0d-45c1-8f9b-dc4975d0bfc8', 'PROFESSOR'),
+       ('9b6faf8f-c187-4198-82f4-93236fc26446', 'GRADUATE'),
+       ('2d5da6fc-9fb4-45a1-9e52-5e20a04a3c43', 'GRADUATE'),
+       ('add37e0a-2748-4fcb-bb24-149c2eac825e', 'ADMIN'),
+       ('39d1393f-6b0c-479f-8e54-be5785c7db36', 'GRADUATE'),
+       ('d852e774-8199-4824-8cdb-bf092a00e96c', 'PROFESSOR'),
 
-       ('4f08fb64-603c-499d-bf16-f850d4a7095c', 'PROFESSOR'),
+       ('3ea29a7d-15de-40b0-b79c-fc440d5f85e4', 'PROFESSOR'),
 
-       ('3cd93430-855e-43ab-a7a0-f5af573b4c2d', 'GRADUATE'),
-       ('3aac5c83-7a36-4ef4-93e3-e0e211b56ffa', 'GRADUATE'),
-       ('2785a7e0-1789-4d6d-bdae-dd367ef6b397', 'GRADUATE'),
-       ('23757452-2ea8-4b2d-8caa-72f7f7bc6936', 'PROFESSOR'),
+       ('5f1545bc-7435-4468-a365-e8a625a02fab', 'GRADUATE'),
+       ('fdcb1d4d-636e-429c-b7ba-ecc7af3ef05e', 'GRADUATE'),
+       ('041f4fab-89c4-4198-87c9-7fd1504eeb6d', 'GRADUATE'),
+       ('e405ae18-b349-4d2b-8156-154091b20ec4', 'PROFESSOR'),
 
-       ('2128f4f3-009b-45ee-af99-cd921f5e1d72', 'PROFESSOR'),
+       ('1ba0bd22-062a-4397-abdd-226ca952d67e', 'PROFESSOR'),
 
-       ('06c2a675-71d3-4594-95e2-9bab0817f35e', 'GRADUATE'),
-       ('f8070687-cf08-4b59-bcca-016e992bd5f4', 'PROFESSOR')
+       ('d79b4d52-00ba-4fc9-985d-f00642c131a9', 'GRADUATE'),
+
+       ('add37e0a-2748-4fcb-bb24-149c2eac825e', 'GRADUATE'),
+       ('add37e0a-2748-4fcb-bb24-149c2eac825e', 'PROFESSOR')
 ;
 
 
 
 insert into advisor (id, created_at, user_id)
-values (gen_random_uuid(), now(), 'b706727c-786c-4daa-9e84-946451946dbc'),
-       (gen_random_uuid(), now(), '8005e55a-8d0d-45c1-8f9b-dc4975d0bfc8'),
-       (gen_random_uuid(), now(), '4f08fb64-603c-499d-bf16-f850d4a7095c'),
-       (gen_random_uuid(), now(), '23757452-2ea8-4b2d-8caa-72f7f7bc6936'),
-       (gen_random_uuid(), now(), '2128f4f3-009b-45ee-af99-cd921f5e1d72'),
-       (gen_random_uuid(), now(), 'f8070687-cf08-4b59-bcca-016e992bd5f4')
+values ('3235fe8b-24de-4ad6-9aad-5f0af4dfbdcb', now(), 'b9306ad8-9713-4b9c-ad87-39570e4e8b85'),
+       ('7d4020dd-aab5-4722-b919-17bb44298c70', now(), 'd852e774-8199-4824-8cdb-bf092a00e96c'),
+       ('c31f794e-a715-414d-8b8e-71307e026a6b', now(), '3ea29a7d-15de-40b0-b79c-fc440d5f85e4'),
+       ('e588ac1f-cc7c-47f8-8698-22f73d0e056a', now(), 'e405ae18-b349-4d2b-8156-154091b20ec4'),
+       ('7bb4e8d9-6678-4b28-bcd4-b4973da16de4', now(), '1ba0bd22-062a-4397-abdd-226ca952d67e'),
+       ('6dfc1412-f9ca-46d3-9c0e-adc3d3a46d61', now(), 'add37e0a-2748-4fcb-bb24-149c2eac825e')
 ;
 
-insert into graduate (id, created_at, history_status_id, user_id)
-values (gen_random_uuid(), now(), '39393f16-f0ab-4ab1-8e51-89704470e8e0', 'fe52dbee-c4e2-46b3-b48a-2292a1a3e84b'),
-       (gen_random_uuid(), now(), 'bfccb1cc-7d0c-4766-b595-e7e90d8deae6', 'f9194fdf-43b4-4730-ae79-f529f94c7036'),
-       (gen_random_uuid(), now(), '601b7636-f8e5-454a-89a3-1f63a6474298', 'f8070687-cf08-4b59-bcca-016e992bd5f4'),
-       (gen_random_uuid(), now(), null, 'f6336a47-33d3-473c-921c-7818f892158d'),
-       (gen_random_uuid(), now(), 'e7e6b627-8b37-45ba-a71f-619dffa11816', 'ed42630c-ff94-47da-a727-5a0316b9d320'),
-       (gen_random_uuid(), now(), null, 'e629f2d2-49b0-49d8-93b9-ead61095c26c'),
-       (gen_random_uuid(), now(), null, '4f08fb64-603c-499d-bf16-f850d4a7095c'),
-       (gen_random_uuid(), now(), null, '3cd93430-855e-43ab-a7a0-f5af573b4c2d'),
-       (gen_random_uuid(), now(), null, '3aac5c83-7a36-4ef4-93e3-e0e211b56ffa'),
-       (gen_random_uuid(), now(), null, '2785a7e0-1789-4d6d-bdae-dd367ef6b397'),
-       (gen_random_uuid(), now(), null, '23757452-2ea8-4b2d-8caa-72f7f7bc6936'),
-       (gen_random_uuid(), now(), null, '2128f4f3-009b-45ee-af99-cd921f5e1d72')
+insert into graduate (id, created_at, user_id)
+values ('9342a6d5-9c24-437e-a44a-cfbb85f74134', now(), 'e668b740-5499-4caf-a5a2-0431ef0a5444'),
+       ('97e93d90-fc9e-4df4-9f0b-05854b58f1f2', now(), '374570af-f7e5-4585-9815-942c20746f06'),
+       ('55c51a44-c9af-40be-bacb-5791a5dae769', now(), 'cf4dedca-1f1b-43dd-af71-3b0f25cb9b9c'),
+       ('7ca270fc-03b6-4796-9c10-43134b018009', now(), 'bee99937-97bb-4d0d-8219-5ac586c6c712'),
+       ('bf23c759-306d-4c40-9238-f3b8769c2f99', now(), 'e69e3415-b1fa-4430-bdca-886fe5bc9af4'),
+       ('157c4d9f-ac1b-47fd-9195-574d25e5d310', now(), '59d8c94f-e5b5-4259-b0d3-2d26758e7eaa'),
+       ('1c2ed564-2939-4227-b657-8324ba591854', now(), '9b6faf8f-c187-4198-82f4-93236fc26446'),
+       ('410275e7-121c-4739-81ad-6164b55fd5c9', now(), '2d5da6fc-9fb4-45a1-9e52-5e20a04a3c43'),
+       ('03a2255f-9236-427f-8ecd-5a89e4140bbe', now(), '39d1393f-6b0c-479f-8e54-be5785c7db36'),
+       ('4a8524ac-6a10-440f-9979-b5922195f22d', now(), '5f1545bc-7435-4468-a365-e8a625a02fab'),
+       ('29c63e7b-f80f-47d5-86d7-41ad5e1627aa', now(), 'fdcb1d4d-636e-429c-b7ba-ecc7af3ef05e'),
+       ('650bb04e-7c8d-47b0-ae30-203a9a6b1c6a', now(), '041f4fab-89c4-4198-87c9-7fd1504eeb6d'),
+       ('4150a939-f66a-4777-a7aa-ef31c239619a', now(), 'd79b4d52-00ba-4fc9-985d-f00642c131a9'),
+       ('0a9c91e3-3094-4d29-a7fd-d203c09fd070', now(), 'add37e0a-2748-4fcb-bb24-149c2eac825e')
 ;
 
 insert into cnpqlevel (id, name, created_at, active)
-values (gen_random_uuid(), 'PQ2', now(), true),
-       (gen_random_uuid(), 'PQ1D', now(), true),
-       (gen_random_uuid(), 'PQ1C', now(), true),
-       (gen_random_uuid(), 'PQ1B', now(), true),
-       (gen_random_uuid(), 'PQ1A', now(), true);
+values ('b2d46fe6-a9df-493a-8f03-f6abcdd3ecc8', 'PQ2', now(), true),
+       ('2813b8c1-9ff8-42f5-8fed-322975666cce', 'PQ1D', now(), true),
+       ('629ce641-4d5f-47a4-95c9-482e5080204c', 'PQ1C', now(), true),
+       ('d067e739-43f8-41ee-b9aa-7be1549e97af', 'PQ1B', now(), true),
+       ('ccc1cef7-62be-4ed8-ae56-4cb46c10db14', 'PQ1A', now(), true);
 
 insert into ci_program (id, initials, created_at, active)
-values (gen_random_uuid(), 'PGC', now(), true),
-       (gen_random_uuid(), 'CAA', now(), true)
+values ('abe12190-7e1a-420b-82ca-1485804361b6', 'PGC', now(), true),
+       ('05ca544c-f3f5-44c3-8a64-41cb8e3b2125', 'CAA', now(), true)
 ;
 
 insert into institution_type (id, name, created_at)
-values (gen_random_uuid(), 'Universidade pública federal', now()),
-       (gen_random_uuid(), 'Instituição pública federal', now()),
-       (gen_random_uuid(), 'Instituição pública estadual', now()),
-       (gen_random_uuid(), 'Instituição privada', now()),
-       (gen_random_uuid(), 'Organização pública', now()),
-       (gen_random_uuid(), 'Empresa ou instituição brasileira', now()),
-       (gen_random_uuid(), 'Empresa ou instituição estrangeira', now()),
-       (gen_random_uuid(), 'Outros', now())
+values ('75286fb2-d03a-4851-bcb6-10467046e1c1', 'Universidade pública federal', now()),
+       ('9b02bcf7-1f60-4878-8af2-975a2e4454b2', 'Instituição pública federal', now()),
+       ('58d230df-a0df-4801-be59-ba9c32f614a7', 'Instituição pública estadual', now()),
+       ('5a9ba5f3-0842-4e87-a620-23350a95d7a1', 'Instituição privada', now()),
+       ('091fbc53-fbe7-4f83-ad95-8e279303914d', 'Organização pública', now()),
+       ('521ea3d9-6361-4444-896a-18ae9c617416', 'Empresa ou instituição brasileira', now()),
+       ('a0a36df1-5475-40ef-9e1c-7ea13711f517', 'Empresa ou instituição estrangeira', now()),
+       ('71b564c5-8cea-44ee-bb22-6d82e8711445', 'Outros', now())
 ;
 
 
 insert into institution (id, created_at, name, type_id)
-values (gen_random_uuid(), now(), 'OLX', '997e504e-bc71-40cf-99da-f4c64cd31448'),
-       (gen_random_uuid(), now(), 'BNDES', '85448e00-8097-426f-af9d-6eb434cce877'),
-       (gen_random_uuid(), now(), 'PWC', '6858aa91-81ef-41ab-928e-30d1f5a75420'),
-       (gen_random_uuid(), now(), 'M4U', '997e504e-bc71-40cf-99da-f4c64cd31448'),
-       (gen_random_uuid(), now(), 'SNC', '997e504e-bc71-40cf-99da-f4c64cd31448'),
-       (gen_random_uuid(), now(), 'Alura', '997e504e-bc71-40cf-99da-f4c64cd31448')
+values ('a7725cf5-bc50-49a8-a7a6-bcd34bd6fb23', now(), 'OLX', '521ea3d9-6361-4444-896a-18ae9c617416'),
+       ('c53ad935-8dc3-4f58-b4c9-6ed0c95e74bf', now(), 'BNDES', '521ea3d9-6361-4444-896a-18ae9c617416'),
+       ('67b39fd5-6352-4bff-b2c4-3f200dce6dcf', now(), 'PWC', 'a0a36df1-5475-40ef-9e1c-7ea13711f517'),
+       ('6051ef32-41a1-4b4e-9f46-67ba8712e396', now(), 'M4U', '521ea3d9-6361-4444-896a-18ae9c617416'),
+       ('a061aa7c-2153-400a-8ff8-d44dcab9caec', now(), 'SNC', '521ea3d9-6361-4444-896a-18ae9c617416'),
+       ('0deb8c15-2877-4171-ab25-16b27852dad3', now(), 'Alura', '71b564c5-8cea-44ee-bb22-6d82e8711445')
 ;
 
 insert into work_history (id, created_at, "position", graduate_id, institution_id, updated_at, started_at)
-values (gen_random_uuid(), now(), 'Dev Jr', '07aa785c-301c-43e4-8286-989950a880f9', '9e13d8c8-d48f-4d34-b1b1-f18bea72598e', null, now()),
-       (gen_random_uuid(), now(), 'Estagiário', 'd6e22b02-da74-4a52-b69c-b8a178754412', 'b0a7b1f1-9544-484b-88f6-9bfc948a9427', null, now()),
-       (gen_random_uuid(), now(), 'Estagiário', '07aa785c-301c-43e4-8286-989950a880f9', 'b0a7b1f1-9544-484b-88f6-9bfc948a9427', null, now()),
-       (gen_random_uuid(), now(), 'Senior Associate', '94b55af5-82dd-440f-aa57-d60916442bf1', 'b5f3d4a0-c2f6-4994-874f-825afdf1db50', null, now()),
-       (gen_random_uuid(), now(), 'Dev Jr', '07aa785c-301c-43e4-8286-989950a880f9', 'd9dfce9e-0b89-4074-8fc6-1294c19ea4e8', null, now()),
-       (gen_random_uuid(), now(), 'Dev Jr', '94b55af5-82dd-440f-aa57-d60916442bf1', '22bcad2a-333d-4a20-bb6f-7d199e1b05f5', null, now())
+values ('81fa7541-3b48-4e6b-b776-6d9d62fd513c', now(), 'Dev Jr', '9342a6d5-9c24-437e-a44a-cfbb85f74134',
+        'a7725cf5-bc50-49a8-a7a6-bcd34bd6fb23', null, now()),
+       ('c36da1a8-639d-4ab6-af09-5c693d097ff4', now(), 'Estagiário', '97e93d90-fc9e-4df4-9f0b-05854b58f1f2',
+        'c53ad935-8dc3-4f58-b4c9-6ed0c95e74bf', null, now()),
+       ('042a5be8-84ea-499c-a0a6-73baa46b43a7', now(), 'Estagiário', '55c51a44-c9af-40be-bacb-5791a5dae769',
+        '67b39fd5-6352-4bff-b2c4-3f200dce6dcf', null, now()),
+       ('9e2f5802-fc34-4468-b129-c2821f659bdc', now(), 'Senior Associate', '7ca270fc-03b6-4796-9c10-43134b018009',
+        '6051ef32-41a1-4b4e-9f46-67ba8712e396', null, now()),
+       ('27fd8198-2ab2-4553-83cf-1133fc5c814c', now(), 'Dev Jr', 'bf23c759-306d-4c40-9238-f3b8769c2f99',
+        'a061aa7c-2153-400a-8ff8-d44dcab9caec', null, now()),
+       ('153c982d-d39f-4c93-bd67-e69cd2668402', now(), 'Dev Jr', '157c4d9f-ac1b-47fd-9195-574d25e5d310',
+        '0deb8c15-2877-4171-ab25-16b27852dad3', null, now())
 ;
 
 insert into course (id, created_at, minute_defense, program_id, advisor_id, graduate_id)
-values (gen_random_uuid(), now(), 1, 'f1643b6d-0036-4982-9635-39a7029bfc3b', '3e853481-d5da-4984-ab5c-30d3aea21c39', '07aa785c-301c-43e4-8286-989950a880f9'),
-       (gen_random_uuid(), now(), 2, '9ddf6187-9f6e-4ba5-b425-3baed3ba1d4e', 'a4cb905d-96c3-44bc-afd8-80ea87ee9a62', '94b55af5-82dd-440f-aa57-d60916442bf1'),
-       (gen_random_uuid(), now(), 3, 'f1643b6d-0036-4982-9635-39a7029bfc3b', 'f5f56998-6ca6-421e-a3e4-ac401d661d0f', '3020232d-cae9-4dc8-b443-cf2ff45e4d21'),
-       (gen_random_uuid(), now(), 4, 'f1643b6d-0036-4982-9635-39a7029bfc3b', 'da3ede74-eb2a-40b6-b966-8da2c75dbaa2', '91cccaa0-d488-4669-8aec-61a0fccd3b2c'),
-       (gen_random_uuid(), now(), 5, '9ddf6187-9f6e-4ba5-b425-3baed3ba1d4e', '1d943fb3-79af-4d93-8cd0-acccf56d90c3', 'd6e22b02-da74-4a52-b69c-b8a178754412'),
-       (gen_random_uuid(), now(), 6, '9ddf6187-9f6e-4ba5-b425-3baed3ba1d4e', '06a9b3ef-969f-4294-a1c1-1c610b708a70', '38e6aaeb-4d3c-4471-a5fa-f9686eeef110')
+values ('5a54864d-3e27-446f-ba98-b69dfdd419b0', now(), 1, 'abe12190-7e1a-420b-82ca-1485804361b6', '3235fe8b-24de-4ad6-9aad-5f0af4dfbdcb',
+        '9342a6d5-9c24-437e-a44a-cfbb85f74134'),
+       ('a91c50e1-3ac8-4b67-b736-44cab7784287', now(), 2, 'abe12190-7e1a-420b-82ca-1485804361b6', '7d4020dd-aab5-4722-b919-17bb44298c70',
+        '97e93d90-fc9e-4df4-9f0b-05854b58f1f2'),
+       ('b7cbafd1-5d90-4902-8a40-032430591368', now(), 3, 'abe12190-7e1a-420b-82ca-1485804361b6', 'c31f794e-a715-414d-8b8e-71307e026a6b',
+        '55c51a44-c9af-40be-bacb-5791a5dae769'),
+       ('0ed7f832-4e44-481d-ace6-f18a4d353135', now(), 4, '05ca544c-f3f5-44c3-8a64-41cb8e3b2125', '3235fe8b-24de-4ad6-9aad-5f0af4dfbdcb',
+        '7ca270fc-03b6-4796-9c10-43134b018009'),
+       ('80482917-c71b-49df-b603-3228c68a3bd0', now(), 5, '05ca544c-f3f5-44c3-8a64-41cb8e3b2125', 'e588ac1f-cc7c-47f8-8698-22f73d0e056a',
+        'bf23c759-306d-4c40-9238-f3b8769c2f99'),
+       ('e97e03b8-b925-4d77-89b7-35c1c90df393', now(), 6, '05ca544c-f3f5-44c3-8a64-41cb8e3b2125', '7bb4e8d9-6678-4b28-bcd4-b4973da16de4',
+        '157c4d9f-ac1b-47fd-9195-574d25e5d310')
 ;
 
 insert into email (id, title, name, content, button_text, button_url, is_graduate_email, active, created_at)
-values (gen_random_uuid(), 'Primeiro Email', 'Primeiro nome', 'Esse ;e o conteúdo do primeiro email', 'Clique Aqui', 'www.google.com',
+values ('b8d031a9-ac66-4945-a958-45bce51448b8', 'Primeiro Email', 'Primeiro nome', 'Esse é o conteúdo do primeiro email', 'Clique Aqui',
+        'www.google.com',
         true, true, now()),
-       (gen_random_uuid(), 'Segundo Email', 'Segundo nome', 'Esse ;e o conteúdo do segundo email', 'Clique Aqui', 'www.google.com',
+       ('f72b3795-eacc-4a74-8e0b-330da3ef586e', 'Segundo Email', 'Segundo nome', 'Esse é o conteúdo do segundo email', 'Clique Aqui',
+        'www.google.com',
         false, true, now())
 ;
