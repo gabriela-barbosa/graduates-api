38a0a316be2ee050766f84beb0eb39fceeb2d30f
gabriela-barbosa <gabrielabf@id.uff.br>
2023-06-28 11:16:05 -0300

    Add front host env variable

--- src/main/kotlin/br/uff/graduatesapi/config/WebConfig.kt
+++ src/main/kotlin/br/uff/graduatesapi/config/WebConfig.kt
@@ -1,5 +1,6 @@
 package br.uff.graduatesapi.config
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.EnableWebMvc
@@ -8,10 +9,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 @Configuration
 @EnableWebMvc
 class WebConfig : WebMvcConfigurer {
+    @Value("\${front-host}")
+    private lateinit var frontHost: String
     override fun addCorsMappings(registry: CorsRegistry) {
         registry.addMapping("/**")
             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT")
-            .allowedOrigins("http://localhost:3000")
+            .allowedOrigins(frontHost)
             .allowCredentials(true)
     }
 }
