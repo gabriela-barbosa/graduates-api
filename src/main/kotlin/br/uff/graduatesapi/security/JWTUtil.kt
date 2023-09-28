fc7b0c4f6011cf12e85e6ee4e6359d6ddacdbfee
gabriela-barbosa <gabrielabf@id.uff.br>
2023-09-28 08:49:22 -0300

    feature: remove token expiration

--- src/main/kotlin/br/uff/graduatesapi/security/JWTUtil.kt
+++ src/main/kotlin/br/uff/graduatesapi/security/JWTUtil.kt
@@ -5,7 +5,6 @@ import io.jsonwebtoken.Jwts
 import io.jsonwebtoken.SignatureAlgorithm
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
-import java.util.*
 import javax.servlet.http.Cookie
 
 @Component
@@ -14,17 +13,12 @@ class JWTUtil {
     @Value("\${jwt.secret}")
     private lateinit var secret: String
 
-    @Value("\${token.expiration}")
-    private lateinit var expiration: String
-
     fun parseJwtToBody(jwt: String): Claims = Jwts.parser().setSigningKey(secret).parseClaimsJws(jwt).body
 
     fun generateJWT(issuer: String): String {
-        val exp = expiration.toLong()
 
         return Jwts.builder()
             .setIssuer(issuer)
-            .setExpiration(Date(System.currentTimeMillis() + exp))
             .signWith(SignatureAlgorithm.HS512, secret).compact()
     }
 
